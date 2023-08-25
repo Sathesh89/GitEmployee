ec043678fbe702c05392952ed45d7174c706147d
Thikazh@18 <rp.vinotha@gmail.com>
2023-08-25 20:00:25 +0530

    Taskcompleted

---
+++ src/com/org/Employeee.java
@@ -0,0 +1,14 @@
+package com.org;
+
+import com.pojo.Employee;
+
+public class Employeee {
+public void add() {
+
+System.out.println("add");
+
+}
+public static void main(String[] args) {
+	Employee e=new Employee();
+}
+}
